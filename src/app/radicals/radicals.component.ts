c7629e8b64547791e75c94adbe001e5ee5f3de48
EricGarrison <egarrisn@swbell.net>
2022-04-19 13:57:50 -0500

    Add Radicals alliance

---
+++ src/app/radicals/radicals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-radicals',
+  templateUrl: './radicals.component.html',
+  styleUrls: ['./radicals.component.css']
+})
+export class RadicalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
