a7bd1c0575c7106924126588564189315cc76592
EricGarrison <egarrisn@swbell.net>
2022-05-12 00:18:10 -0500

    Add rules for basic version of ATW

---
+++ src/app/basic-rules/basic-rules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-rules',
+  templateUrl: './basic-rules.component.html',
+  styleUrls: ['./basic-rules.component.css']
+})
+export class BasicRulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
